68172e37d3de80a67199ddc8003330cb42c0774f
LAPTOP-4RQNS55P\lenovo <thojuddy@gmail.com>
2021-09-29 21:01:23 +0700

    first commit

---
+++ Demo.Web/Pages/FetchData.razor.cs
@@ -0,0 +1,20 @@
+﻿using Demo.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Pages
+{
+    public partial class FetchData
+    {
+        
+    private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        
+    }
+}
+}
---
+++ Demo.Web/Pages/FetchDataBase.cs
@@ -0,0 +1,22 @@
+﻿using Demo.Web.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Demo.Web.Pages
+{
+    public class FetchDataBase : ComponentBase
+    {
+        public WeatherForecast[] forecasts;
+
+      [Inject] WeatherForecastService   ForecastService { get;  set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+
+        } 
+
+    }
+}
